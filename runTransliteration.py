c5aea588ed036e036e1f936daca743ce278d9b88
ritwikmishra <ritwik.m07@gmail.com>
2019-10-20 23:53:14 +0530

    updated addition of trailing 'a'

--- runTransliteration.py
+++ runTransliteration.py
@@ -21,25 +21,28 @@ file.close()
 str1 = ""
 
 for x in content:
-	for i in range(len(x)):
-		if (i+1<len(x) and x[i+1].strip()==' ़'.strip()):
-			c = x[i]+x[i+1]
-			p=2
-		else:
-			c = x[i]
-			p=1
-		if (c in vowels.keys()):
-			str1 = str1 + vowels[c]
-		elif (c in consonants.keys()):
-			if(i+p<len(x) and x[i+p] in consonants.keys()):
-				if(c=='झ' and i!=0) or (c == 'ॠ'): # add 'a' after 'jh', only if झ appears in the starting of the word
-					str1 = str1 + consonants[c]
-				else:
-					str1 = str1 + consonants[c]+'a'
+	for y in x.split():
+		for i in range(len(y)):
+			if (i+1<len(y) and y[i+1].strip()==' ़'.strip()):
+				c = y[i]+y[i+1]
+				p=2
 			else:
-				str1 = str1 + consonants[c]
-		elif x[i] in ['\n','\t',' ','!',',','।','-',':'] or c.isalnum():
-			str1 = str1 + c.replace('।','.')
+				c = y[i]
+				p=1
+			if (c in vowels.keys()):
+				str1 = str1 + vowels[c]
+			elif (c in consonants.keys()):
+				if(i+p<len(y) and y[i+p] in consonants.keys()):
+					if ((c=='झ' and i!=0) or (i!=0 and i+p+1<len(y) and y[i+p+1] in vowels.keys())): # add 'a' after 'jh', only if झ appears in the starting of the word
+						str1 = str1 + consonants[c]
+					else:
+						str1 = str1 + consonants[c]+'a'
+				else:
+					str1 = str1 + consonants[c]
+			elif y[i] in ['\n','\t',' ','!',',','।','-',':','\\','_'] or c.isalnum():
+				str1 = str1 + c.replace('।','.')
+		str1 = str1 + " "
+	str1 = str1 + "\n"
 
 file = open(sys.argv[1]+'OUTPUT','w')
 file.write(str1.strip())
